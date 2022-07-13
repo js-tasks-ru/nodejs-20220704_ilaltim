2a7f0f9317c3aef5d630f1a4c129fdfc6813b054
Ilya Edelshtein <ilya.edelstein@konrnit.com>
2022-07-13 11:03:03 +0300

    fixes: code review improvements

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -10,7 +10,8 @@ class LimitSizeStream extends stream.Transform {
   _transform(chunk, encoding, callback) {
     const chunkSize = chunk.byteLength;
     if (chunkSize > this.limit) {
-      this.destroy(new LimitExceededError());
+      // this.destroy(new LimitExceededError());
+      return callback(new LimitExceededError(), null);
     }
     this.limit = this.limit - chunkSize;
     callback(null, chunk);
